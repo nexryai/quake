0d3a8e581ef1ec4c7be1efd7874d023ed5672567
nexryai <nexryai@sda1.net>
2025-03-04 18:03:24 +0900

    Update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,7 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
+import pluginImport from "eslint-plugin-import";
+import pluginVue from "eslint-plugin-vue";
+import pluginTypescript from "typescript-eslint";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -7,16 +9,63 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+    {
+        name: "app/files-to-lint",
+        files: ["**/*.{ts,mts,tsx,vue}"],
+    },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+    {
+        name: "app/files-to-ignore",
+        ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+    },
 
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-)
+    pluginVue.configs["flat/essential"],
+    vueTsConfigs.recommended,
+    pluginImport.flatConfigs.recommended,
+    pluginImport.flatConfigs.typescript,
+
+    {
+        files: ["**/*.vue", "**/*.ts", "**/*.js"],
+
+        languageOptions: {
+            parserOptions: {
+                parser: pluginTypescript.parser
+            }
+        },
+        rules: {
+            "@typescript-eslint/no-unused-expressions": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "indent": ["error", 4],
+            "quotes": ["error", "double"],
+            "semi": ["error", "always"],
+            "import/no-cycle": "error",
+            // https://github.com/import-js/eslint-plugin-import/issues/2765
+            "import/no-unresolved": "off",
+            "import/no-named-as-default": "off",
+            "import/order": [
+                "error",
+                {
+                    groups: ["builtin", "external", "parent", "sibling", "index", "object"],
+                    pathGroups: [
+                        {
+                            pattern: "{svelte,$app/**,elysia}",
+                            group: "builtin",
+                            position: "before",
+                        },
+                        {
+                            pattern: "{@src/**,$lib/**,@prisma/client}",
+                            group: "parent",
+                            position: "before",
+                        },
+                    ],
+                    pathGroupsExcludedImportTypes: ["type"],
+                    alphabetize: {
+                        order: "asc",
+                    },
+                    "newlines-between": "always",
+                },
+            ],
+        }
+    },
+);
