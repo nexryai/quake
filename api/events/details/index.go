1fc496732e91c8050f4984bc0ce8030ec85f14fe
nexryai <nexryai@sda1.net>
2025-03-05 14:26:36 +0900

    api: add debug flag

--- api/events/details/index.go
+++ api/events/details/index.go
@@ -8,6 +8,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	debug := false
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	queryParams := r.URL.Query().Get("id")
@@ -16,7 +17,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data, err := controller.GetEventDetailsJson(queryParams)
+	debugFlag := r.URL.Query().Get("debug")
+	if debugFlag == "dummy" {
+		debug = true
+	}
+
+	data, err := controller.GetEventDetailsJson(queryParams, debug)
 	if err != nil || data == nil {
 		w.WriteHeader(400)
 		return
